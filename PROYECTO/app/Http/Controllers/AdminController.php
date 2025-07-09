2451f1918ca7dba37f8dd47965ae56869bd27f07
Oskar Jurado <oskar@jurado.com>
2025-07-08 20:59:33 -0500

    Arreglado el admin puede registrar areas, niveles, titulaciones y asginaturas, tiene el error en areas nose porque ya me cae mal estar haciendo eso acada rato

--- PROYECTO/app/Http/Controllers/AdminController.php
+++ PROYECTO/app/Http/Controllers/AdminController.php
@@ -16,4 +16,6 @@ class AdminController extends Controller
 
         return view('Admin', compact('totalUsuarios', 'totalAreas', 'totalRoles'));
     }
+
+    
 }
---
+++ PROYECTO/resources/views/asignaturas/edit.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Editar Asignatura</title>
+</head>
+<body>
+    <h1>Editar Asignatura: {{ $asignatura->nombreasi }}</h1>
+
+    <a href="{{ route('asignaturas.index') }}">Volver al listado</a>
+
+    @if ($errors->any())
+        <div style="color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('asignaturas.update', $asignatura->idasi) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <label>Titulación:</label>
+        <select name="idtit" required>
+            <option value="">-- Seleccione una titulación --</option>
+            @foreach ($titulaciones as $tit)
+                <option value="{{ $tit->idtit }}" {{ $asignatura->idtit == $tit->idtit ? 'selected' : '' }}>{{ $tit->detallet }}</option>
+            @endforeach
+        </select><br><br>
+
+        <label>Nivel:</label>
+        <select name="idniv" required>
+            <option value="">-- Seleccione un nivel --</option>
+            @foreach ($niveles as $niv)
+                <option value="{{ $niv->idniv }}" {{ $asignatura->idniv == $niv->idniv ? 'selected' : '' }}>{{ $niv->nombreniv }}</option>
+            @endforeach
+        </select><br><br>
+
+        <label>Nombre de la asignatura:</label>
+        <input type="text" name="nombreasi" value="{{ $asignatura->nombreasi }}" required><br><br>
+
+        <label>Horas teóricas:</label>
+        <input type="number" name="teoricosasi" value="{{ $asignatura->teoricosasi }}" min="0" required><br><br>
+
+        <label>Horas prácticas:</label>
+        <input type="number" name="practicosasi" value="{{ $asignatura->practicosasi }}" min="0" required><br><br>
+
+        <button type="submit">Actualizar</button>
+    </form>
+</body>
+</html>
